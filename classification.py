80a384434c77f3854141cad70a4c45ca715ac7a7
Митрофанов Илья Алексеевич <IMitrofanov@int.kpfu.ru>
2026-05-07 15:55:03 +0300

    классифирую изображения, поступающие через брокер

--- classification.py
+++ classification.py
@@ -1,18 +1,25 @@
+import io
 import os.path
 
 import joblib
 from PIL import Image
 import numpy as np
+import json
+
 
 class WaterClassifier:
     def __init__(self):
         self.model = joblib.load('water_classifier.pkl')
         self.label_encoder = joblib.load('water_labels.pkl')
 
-    def predict(self, img_path):
+    def predict(self, image):
 
-        img = Image.open(os.path.join('test', img_path)).convert("RGB")
-        img = img.resize((128, 128))
+        imgJSONString = image.decode('utf-8')
+        imgJSON = json.loads(imgJSONString)
+        print(type(bytearray(imgJSON['data']['buffer']['data'])))
+        imgAnotherByteArray = bytearray(imgJSON['data']['buffer']['data'])
+        anotherImg = Image.open(io.BytesIO(imgAnotherByteArray))
+        img = anotherImg.resize((128, 128))
         X = np.array(img).flatten()
 
         X = X / 255.0
--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from pathlib import Path
 
 from classification import WaterClassifier
 from learning import learn
+import pika
+import sys
+import os
+import base64
+import io
 
-if __name__ == "__main__":
+def main():
 
     requiredFiles = ['water_classifier.pkl', 'water_labels.pkl']
 
@@ -19,5 +24,28 @@ if __name__ == "__main__":
         print('Модель уже обучена')
 
     water_classifier = WaterClassifier()
-    res = water_classifier.predict('IMG_0877.JPG')
-    print(res['position'])
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='water_classification')
+
+    def callback(ch, method, properties, body):
+        # img = base64.b64decode(body)
+        res = water_classifier.predict(body)
+        print(res['position'])
+
+    channel.basic_consume(queue='water_classification', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
